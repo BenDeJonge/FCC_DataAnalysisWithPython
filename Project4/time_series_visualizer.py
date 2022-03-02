49d4a611b37cf3b7de28b0572cafcf930e09b070
Ben De Jonge (imec) <74603987+BenDeJonge@users.noreply.github.com>
2022-03-02 16:59:30 +0100

    Implemented draw_line_plot
    First part of project 4 solved

--- Project4/time_series_visualizer.py
+++ Project4/time_series_visualizer.py
@@ -5,24 +5,32 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv(filepath_or_buffer='Project4/fcc-forum-pageviews.csv',
+                 index_col=0)
 
-# Clean data
-df = None
+# Clean data. Remove datapoints outside of the 2.5% and 97.5% percentiles.
+df = df[ 
+        df.value.between( df.value.quantile(0.025), df.value.quantile(0.975) )
+        ]
+df.index = pd.to_datetime(df.index, infer_datetime_format='%YYYY-&MM-%DD')
 
-
-def draw_line_plot():
-    # Draw line plot
-    fig = None
-
-
-
-
+def draw_line_plot(df):
+    # Object-oriented Matplotlib interface, separating the figure from the axis.
+    fig, ax = plt.subplots()
+    # Labelling both axes.
+    ax.set_ylabel('Page Views')
+    ax.set_xlabel('Date')
+    # Formatting a title.
+    start, end = [ date.strftime('%m/%Y') for date in (df.index[0], df.index[-1]) ]
+    ax.set_title(f'Daily Free Code Camp Forum Page Views {start}-{end}')
+    # Plotting the data.
+    ax.plot(df, color='r')
     # Save image and return fig (don't change this part)
+    fig.set_size_inches((16, 4))
     fig.savefig('Project4/line_plot.png', bbox_inches='tight', transparent=True)
     return fig
 
-def draw_bar_plot():
+def draw_bar_plot(df):
     # Copy and modify data for monthly bar plot
     df_bar = None
     fig = None
@@ -52,3 +60,5 @@ def draw_box_plot():
     # Save image and return fig (don't change this part)
     fig.savefig('Project4/box_plot.png', bbox_inches='tight', transparent=True)
     return fig
+
+draw_line_plot(df)
